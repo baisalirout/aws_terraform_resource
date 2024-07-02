51284c13065af6337c8ef6fdf4cf9debd8f7a447
baisalirout <baisalirout@gmail.com>
2024-07-01 20:37:04 -0400

    codeRefactor

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "instance_ip_address" {
-  value = aws_instance.terraform_created[0].private_ip
+  value = aws_instance.canada.private_ip
 
 
 }
 output "instance_publicip_address" {
-  value = aws_instance.terraform_created[0].public_ip
+  value = aws_instance.NVgnia.public_ip
 }
