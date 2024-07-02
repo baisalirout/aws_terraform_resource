6f543b5619cdaa6f09e30f97477b10d14c71f216
baisalirout <baisalirout@gmail.com>
2024-07-01 20:40:13 -0400

    chnage

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "instance_ip_address" {
-  value = aws_instance.canada.private_ip
+  value = aws_instance.canada[0].private_ip
 
 
 }
 output "instance_publicip_address" {
-  value = aws_instance.NVgnia.public_ip
+  value = aws_instance.NVgnia[0].public_ip
 }
